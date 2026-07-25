3d5c98a3cbc2620a838119505d3f64140ef32280
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-07-25 12:19:26 +0100

    Track KV3 decode results in the resource graph

--- src/VMSourceCONV/graph/ResourceGraph.h
+++ src/VMSourceCONV/graph/ResourceGraph.h
@@ -5,9 +5,14 @@
 #include <cstddef>
 #include <cstdint>
 #include <filesystem>
+#include <memory>
 #include <optional>
 #include <string>
 #include <vector>
+
+namespace vmsourceconv::serialization::kv3 {
+struct Kv3Document;
+}
 
 namespace vmsourceconv::graph {
 
@@ -33,6 +38,19 @@ struct ResourceBlockSummary {
     std::uint32_t size = 0;
 };
 
+struct Kv3DecodeSummary {
+    bool attempted = false;
+    bool succeeded = false;
+    std::string message;
+    std::string rootType;
+    std::size_t stringCount = 0;
+    std::size_t nodeCount = 0;
+    std::size_t objectCount = 0;
+    std::size_t arrayCount = 0;
+    std::size_t binaryBlobCount = 0;
+    std::size_t maximumDepth = 0;
+};
+
 struct ResourceGraphNode {
     std::size_t index = 0;
     std::optional<std::size_t> parentIndex;
@@ -52,6 +70,8 @@ struct ResourceGraphNode {
     std::uint16_t resourceVersion = 0;
     std::vector<ResourceBlockSummary> blocks;
     inspect::data::DataInspectionResult dataInspection;
+    Kv3DecodeSummary kv3Decode;
+    std::shared_ptr<serialization::kv3::Kv3Document> kv3Document;
     std::size_t externalReferenceCount = 0;
     std::size_t warningCount = 0;
     std::size_t errorCount = 0;
@@ -67,6 +87,9 @@ struct ResourceGraphStatistics {
     std::size_t resourcesLoadedFromVpk = 0;
     std::size_t resourcesMissing = 0;
     std::size_t resourcesFailed = 0;
+    std::size_t kv3DocumentsDecoded = 0;
+    std::size_t kv3DocumentsFailed = 0;
+    std::size_t entityDocumentsRetained = 0;
     bool resourceLimitReached = false;
 };
 
